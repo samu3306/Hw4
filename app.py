1569d36a0a5676f56b8626376dab0d0f05f75c6d
samu3306 <coeall21809@gmail.com>
2025-05-16 13:57:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ import os
 
 app = Flask(__name__)
 
-#儲存歷史對話
+#歷史對話
 message_history = []
 
-# 替換成你的 LINE Bot token 和 secret
+#LINE Bot token 和 secret
 ACCESS_TOKEN = 'Pgo+htulCicresZzk4EOQFwqDGNauaAt2SvjaAc6frVNO1QTBj3CfL495pOVkLyp03hGLDddSEfHLMcCkt3QmUn8E8dNqDSD35Uyr3t+4mFHUI/7DPQvsjJl0Pra0xqTZrogfB+E1uyjmgfkmxsIcAdB04t89/1O/w1cDnyilFU='
 CHANNEL_SECRET = 'c9d3bf4f2296c16b417e048415f3cbbd'
 GEMINI_API_KEY = 'AIzaSyBtZCKBXy4vUUMWuJ4A5Q2JGpxeNQ09uVI'
@@ -58,14 +58,14 @@ def get_gemini_response(user_message):
     response = requests.post(url, headers=headers, data=json.dumps(data))
 
     if response.status_code != 200:
-        return "AI 無法處理請求，請稍後再試。"
+        return "Error"
 
     try:
         result = response.json()
         reply_text = result['candidates'][0]['content']['parts'][0]['text']
         return reply_text
     except Exception:
-        return "AI 回應異常，請稍後再試。"
+        return "Error"
 
 #LINE 文字訊息
 @handler.add(MessageEvent, message=TextMessage)
@@ -136,5 +136,5 @@ def delete_messages():
 
 # 啟動伺服器
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render 會提供 PORT 環境變數
+    port = int(os.environ.get("PORT", 5000)) 
     app.run(host="0.0.0.0", port=port)
